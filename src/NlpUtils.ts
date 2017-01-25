6dc1ce8ed74c45d245aab4876ec53604c20490a1
rotemdan <rotemdan@gmail.com>
2017-01-25 18:13:16 +0200

    Added tilde (`) as a word end matcher

--- src/NlpUtils.ts
+++ src/NlpUtils.ts
@@ -12,7 +12,7 @@ namespace SpeakTheWeb {
 		// Try to match up to the next punctuation character that is very likely to be a word
 		// Boundary. The MS engines treat parts of abberviations like M.A. as separate words
 		// so this would work with them as well.
-		const wordEndMatch = /--|[\s—"“”@&\^\.\,\;\:\(\)\[\]\{\}\<\>\=\?\!\$\*\%\/\\]|$/.exec(wordAndRemainingText);
+		const wordEndMatch = /--|[\s—"“”@&`\^\.\,\;\:\(\)\[\]\{\}\<\>\=\?\!\$\*\%\/\\]|$/.exec(wordAndRemainingText);
 
 		if (wordEndMatch == null) {
 			return wordStartOffset;
