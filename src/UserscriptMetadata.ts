9a34218e83a005be004bddbb258b8b54608c54b0
rotemdan <rotemdan@gmail.com>
2017-01-26 16:32:44 +0200

    Updated metadata

--- src/UserscriptMetadata.ts
+++ src/UserscriptMetadata.ts
@@ -1,10 +1,11 @@
 // ==UserScript==
 // @name        Speak the Web
-// @namespace   AAA
-// @description Speak any text on the page.
+// @namespace   https://github.com/rotemdan
+// @description Speak any text on the page by middle clicking it.
+// @author      Rotem Dan <rotemdan@gmail.com>
 // @include     http://*
 // @include     https://*
-// @version     0.1.0
+// @version     0.1.1
 // @grant       GM_getValue
 // @grant       GM_setValue
 // @require     https://code.jquery.com/jquery-3.1.1.min.js
